02ef27cd3cf423d4e8c32c41056434c7ef876a5e
abdelrahman99 <ge49kos@mytum.de>
2025-02-12 02:42:58 +0100

    changed workers

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "cluster" {
   source = "./modules"
 
   instance_type     = "c6in.16xlarge" # t2.micro c6in.8xlarge c6in.16xlarge c6gn.16xlarge
-  num_nodes         = 5
+  num_nodes         = 4
   availability_zone = "eu-central-1a"
 
   max_price     = 2
